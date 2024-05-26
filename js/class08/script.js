b60683a91c9c186153f25ddadaa20eb58c9a128e
naveed-rana <rana.naveed812@gmail.com>
2024-05-26 19:01:00 +0500

    assignment12

---
+++ js/class08/script.js
@@ -0,0 +1,68 @@
+
+function sum(a=10,b=20,c=30){
+var result = 0;
+var d = 10;
+if (a% 2 == 0) {
+   result = result + a;
+   
+}
+if (b% 2 == 0) {
+   result = result + b;
+   
+}
+if (c% 2 == 0) {
+   result = result + c;
+   
+}
+result = result + d
+return result
+result = result + 123;
+}
+var num1 = 20
+var num2 = 20;
+
+// var output = sum(num1, 12, 16);
+// var output2 = sum(output, 12, 16);
+// console.log("output",output2);
+
+
+var num5 = "wqasdf";
+console.log("num5", typeof num5, num5);
+var out = parseInt(num5)
+var result = console.log("out",typeof out, out);
+
+// function callSum(){
+//    sum(23, 12, 16);
+// }
+// callSum()
+// for (let index = 0; index < 9; index++) {
+//    sum(index, 12, 16);
+// }
+
+
+
+
+
+
+// console.log("line1")
+// for(var i = 0; i <5; i++) {
+//    console.log("line2")
+// }
+// console.log("line3")
+// if(false){
+// console.log("line4")
+// }
+
+// function abc(){
+// console.log("line5")
+// }
+// console.log("line6")
+// console.log("line7")
+// abc()
+// console.log("line8")
+// console.log("line9")
+
+// console.log("line10")
+// abc();
+// prompt();
+// console.log();

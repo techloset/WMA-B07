fe99c27b9d806b85657225a9fa4d5cf6cd386955
naveed-rana <rana.naveed812@gmail.com>
2024-06-29 19:05:39 +0500

    Asssignment 15

---
+++ js/class11/script.js
@@ -0,0 +1,77 @@
+
+
+
+
+
+// var  addTwoNumbers = (a,b)=> a+b;
+// var nums = [12,123,123,14515,51]
+// var newArr = nums.map((a,i)=> a+i)
+// // var  addTwoNumbers = (a,b)=>{
+// //    return a + b;
+// //    }
+// var result = addTwoNumbers(10,20)
+// console.log("result in line2",result);
+
+// function addTwoNumbers(a,b) {
+//    return a  + b;
+// }
+
+
+// console.log(userName);
+// var userName ="Naveed"
+
+
+
+
+// var nums = [10,21,30,45,50,60]
+
+
+
+
+// function createNewArray(val,i){
+//    if (val % 2 !=0 ) {
+//       return val 
+
+//    }
+// }
+ var newArr = nums.filter((val)=>{
+return val
+ })
+//  var newArr2 = newArr.map(createNewArray)
+// nums.forEach(createNewArray)
+// for (var i = 0; i < nums.length; i++){
+//    newArr.push(nums[i] * 2)
+// }
+// console.log("newArr",newArr);
+
+
+// var studentNames = ["Ali", "Umar",  "Zain", "Abu bakar", "Alam"]
+
+
+
+
+// function getStudentNames(val,index,arr){
+// // console.log("GetStudentNames called",val,index,arr);
+// if (val == "Umar") {
+//    return val + " Failed"
+// }
+// return val + " Passed"
+// }
+
+// var result = studentNames.map(getStudentNames)
+// console.log("result:",result)
+
+// var result = studentNames.forEach(getStudentNames)
+// console.log("result:",result)
+
+// for (var i = 0; i < 5; i++){
+// console.log(studentNames[i])
+// }
+
+
+// function customForEach(arr, func){
+//    for(var i=0; i<arr.length; i++){
+//       func(arr[i],i, arr)
+//    }
+// }
+// customForEach(studentNames, getStudentNames)
